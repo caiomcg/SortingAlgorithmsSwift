05c6d960c0a71995864740f0dd62400adb85dc06
caiomcg <caiomcg@gmail.com>
2019-02-25 17:22:32 -0300

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # SortingAlgorithmsSwift
 
-A description of this package.
+Implementation of the most popular sorting algorithms as swift extensions for the Array data type.
+
+## Requirements
+- [Swift 4.2.2 or above](https://swift.org/download/)
+
+
+## Installing
+
+Below is a quick tutorial on how to install swift on Linux machines, for Mac OS simply install Xcode.
+
+1. Download the bundle containing all swift tools and the language core 
+```sh
+$ wget https://swift.org/builds/swift-4.2.2-release/ubuntu1804/swift-4.2.2-RELEASE/swift-4.2.2-RELEASE-ubuntu18.04.tar.gz
+```
+2. Extract and add the arquive to a shared path on your device
+```sh
+$ tar xzf swift-4.2.2-RELEASE-ubuntu18.04.tar.gz
+$ sudo mv swift-4.2.2-RELEASE-ubuntu18.04 /usr/share/swift
+```
+3. Export the source path on yout system
+```sh
+$ echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
+$ source ~/.bashrc
+```
+4. Check if it is installed
+```sh
+$ swift --version
+```
+
+## Compiling
+
+Compile the source invoking the swift build on the terminal
+```sh
+$ swift build
+```
+
+## Executing
+
+Execute the application by calling swift run with the appropriate parameters
+```sh
+$ swift run SortingAlgorithmsSwift ./instances/num.1000.1.in S
+```
+Where the above will execute selection sort S for the input ./instances/num.1000.1.in
+
+Once concluded the elapsed time will be presented on the terminal and a file named num.1000.1.ALGORITH.out will be written along with the input.
+
+## Contributors
+
+* Caio Marcelo Campoy Guedes <caiomcg@gmail.com>
--- Sources/SortingAlgorithmsSwift/main.swift
+++ Sources/SortingAlgorithmsSwift/main.swift
@@ -6,6 +6,26 @@ extension String {
     }
 }
 
+func usage() {
+    print("\u{001B}[1;37mNAME\u{001B}[0;0m")
+    print("        SortingAlgorithmsSwift - Sorting algorithms written in Swift.")
+    print("\u{001B}[1;37mSYNOPSIS\u{001B}[0;0m")
+    print("        SortingAlgorithmsSwift [INPUT_FILE] [ALGORITHM]")
+    print("\u{001B}[1;37mDESCRIPTION\u{001B}[0;0m")
+    print("        Sort a file with the specified sorting algorithm.")
+    print("        [INPUT_FILE]")
+    print("            File to be sorted" )
+    print("        [ALGORITHM]")
+    print("            Sort with the specified algorith:" )
+    print("            S - Selection Sort" )
+    print("            I - Insertion Sort" )
+    print("\u{001B}[1;37mEXIT STATUS\u{001B}[0;0m")
+    print("        0 - If ok")
+    print("        1 - If a problem occured")
+    print("\u{001B}[1;37mUSE EXAMPLE\u{001B}[0;0m")
+    print("        \u{001B}[1;35mSorting input.txt SI\u{001B}[0;0m")
+}
+
 func dumpToFile(_ arr: [Int64], with name: String) throws {
     let output: String = arr.map({ String($0) }).joined(separator: "\n") // Prepare sorted array to be dumped on the file
     print("Dumping to: \(name)\n")
@@ -14,9 +34,7 @@ func dumpToFile(_ arr: [Int64], with name: String) throws {
 
 jumper: do {
     guard CommandLine.arguments.count >= 3 else {
-        print("Invalid amount of argumenst!\n")
-        print("Usage: SortingAlgorithmsSwift [FILE_PATH] [ALGORITHM]")
-        print("Example: SortingAlgorithmsSwift ./instances/num.1000.1.in S")
+        usage()
         break jumper
     }
 
